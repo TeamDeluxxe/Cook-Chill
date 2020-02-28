e55614c5da79b11c312f1b05e5a8ed5bf2f2f3a1
emiljano7 <46742205+emiljano7@users.noreply.github.com>
2020-02-28 12:53:04 +0100

    Veranstaltungsliste

--- src/app/components/event-list/event-list.component.ts
+++ src/app/components/event-list/event-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Event} from '../../models/Event';
 @Component({
   selector: 'app-event-list',
   templateUrl: './event-list.component.html',
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventListComponent implements OnInit {
 
+  eventList: Array<Event>;
+
   constructor() { }
 
   ngOnInit() {
+    this.eventList=[];
+
+    let e1: Event ={ name: 'Veranstaltung1', allergies: 'Tomate', category: 'Kuchen', eventDate: new Date(2020,11,24), meal: 'StaeK mit kartoffel', price: 10, shop: true, visitNumber: 5, visiter: 3, whoCooks: 'Emiljano', whoShops: 'Gastgeber'};
+    this.eventList.push(e1);
+    let e2: Event ={ name: 'Veranstaltung2', allergies: 'Gurke', category: 'Vegan', eventDate: new Date(2019,10,14), meal: 'Pizza', price: 15, shop: false, visitNumber: 7, visiter: 4, whoCooks: 'Gastgeber', whoShops: 'Nick'};
+    this.eventList.push(e2);
+    let e3: Event ={ name: 'Veranstaltung3', allergies: 'Nuss', category: 'Süßigkeiten', eventDate: new Date(2020,7,12), meal: 'Chips', price: 7, shop: true, visitNumber: 6, visiter: 2, whoCooks: 'Hennig', whoShops: 'Emiljano'};
+    this.eventList.push(e3);
   }
 
 }
---
+++ src/app/models/Event.ts
@@ -0,0 +1,13 @@
+export class  Event{
+  name: string;
+  allergies: string;
+  category: string;
+  eventDate: Date;
+  meal: string;
+  price: number;
+  shop: boolean;
+  visitNumber: number;
+  visiter: number;
+  whoCooks: string;
+  whoShops: string;
+}
