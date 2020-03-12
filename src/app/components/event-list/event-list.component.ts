4aecc4a2c62f43a69362ab253adf16309f45c66e
emiljano7 <46742205+emiljano7@users.noreply.github.com>
2020-03-12 11:39:14 +0100

    Filter Gericht

--- src/app/components/event-list/event-list.component.ts
+++ src/app/components/event-list/event-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Event} from '../../models/Event';
+import {FormControl} from "@angular/forms";
 @Component({
   selector: 'app-event-list',
   templateUrl: './event-list.component.html',
@@ -8,18 +9,35 @@ import {Event} from '../../models/Event';
 export class EventListComponent implements OnInit {
 
   eventList: Array<Event>;
+  filteredEventList: Array<Event>;
+  filterGericht = new FormControl('');
 
   constructor() { }
 
   ngOnInit() {
-    this.eventList=[];
+    this.eventList = [];
+    this.filteredEventList = [];
 
-    let e1: Event ={ name: 'Veranstaltung1', allergies: 'Tomate', category: 'Kuchen', eventDate: new Date(2020,11,24), meal: 'StaeK mit kartoffel', price: 10, shop: true, visitNumber: 5, visiter: 3, whoCooks: 'Emiljano', whoShops: 'Gastgeber'};
+    let e1: Event = { name: 'Veranstaltung1', allergies: 'Tomate', category: 'Kuchen', eventDate: new Date(2020,11,24), meal: 'StaeK mit kartoffel', price: 10, shop: true, visitNumber: 5, visiter: 3, whoCooks: 'Emiljano', whoShops: 'Gastgeber'};
     this.eventList.push(e1);
-    let e2: Event ={ name: 'Veranstaltung2', allergies: 'Gurke', category: 'Vegan', eventDate: new Date(2019,10,14), meal: 'Pizza', price: 15, shop: false, visitNumber: 7, visiter: 4, whoCooks: 'Gastgeber', whoShops: 'Nick'};
+    let e2: Event = { name: 'Veranstaltung2', allergies: 'Gurke', category: 'Vegan', eventDate: new Date(2019,10,14), meal: 'Pizza', price: 15, shop: false, visitNumber: 7, visiter: 4, whoCooks: 'Gastgeber', whoShops: 'Nick'};
     this.eventList.push(e2);
-    let e3: Event ={ name: 'Veranstaltung3', allergies: 'Nuss', category: 'Süßigkeiten', eventDate: new Date(2020,7,12), meal: 'Chips', price: 7, shop: true, visitNumber: 6, visiter: 2, whoCooks: 'Hennig', whoShops: 'Emiljano'};
+    let e3: Event = { name: 'Veranstaltung3', allergies: 'Nuss', category: 'Süßigkeiten', eventDate: new Date(2020,7,12), meal: 'Chips', price: 7, shop: true, visitNumber: 6, visiter: 2, whoCooks: 'Hennig', whoShops: 'Emiljano'};
     this.eventList.push(e3);
+
+    this.doFilter();
+  }
+
+  doFilter() {
+    //this.eventList[0].meal = this.filterGericht.value;
+    this.filteredEventList = this.eventList.slice();
+
+    if(this.filterGericht.value !== ''){
+      
+      // Filterung auf einen passenden Textteil (egal ob GROß oder kleinschreibung)
+      this.filteredEventList = this.filteredEventList.filter(ev => ev.meal.toLowerCase().indexOf( this.filterGericht.value.toString().toLowerCase()) > -1 );
+    }
+
   }
 
 }
